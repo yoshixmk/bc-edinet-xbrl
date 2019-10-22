4b722ade4d8a3a734d19ccc7c3509234e790ac20
YoshihiroUeki <pirori67@gmail.com>
2019-10-23 01:23:44 +0900

    python3.7 downgrade for lxml libraly

--- parse.py
+++ parse.py
@@ -4,12 +4,12 @@ from edinet_xbrl.edinet_xbrl_parser import EdinetXbrlParser
 parser = EdinetXbrlParser()
 
 ## parse xbrl file and get data container
-xbrl_file_path = "C:\\edinet_xbrl\\xbrl\\jpctl010000-icr-001_E01777-000_2019-06-18_01_2019-06-18.xbrl"
+xbrl_file_path = "C:\\edinet_xbrl\\xbrl\\jpcrp040300-q1r-001_E01777-000_2019-06-30_01_2019-08-05.xbrl"
 edinet_xbrl_object = parser.parse_file(xbrl_file_path)
 
 ## get value from container
-key = "jppfs_cor:Assets"
-context_ref = "CurrentYearInstant"
-current_year_assets = edinet_xbrl_object.get_data_by_context_ref(key, context_ref).get_value()
+key = "jpcrp_cor:TotalAssetsUSGAAPSummaryOfBusinessResults"
+context_ref = "Prior1QuarterInstant"
+current_q1_assets = edinet_xbrl_object.get_data_by_context_ref(key, context_ref).get_value()
 
-#print(current_year_assets)
+print(current_q1_assets)
