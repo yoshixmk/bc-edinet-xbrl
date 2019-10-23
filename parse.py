765cea8b8920344f43c80215d5e82fc543f571ac
YoshihiroUeki <pirori67@gmail.com>
2019-10-24 00:57:12 +0900

    key, values output sample

--- parse.py
+++ parse.py
@@ -7,9 +7,14 @@ parser = EdinetXbrlParser()
 xbrl_file_path = "C:\\edinet_xbrl\\xbrl\\jpcrp040300-q1r-001_E01777-000_2019-06-30_01_2019-08-05.xbrl"
 edinet_xbrl_object = parser.parse_file(xbrl_file_path)
 
-## get value from container
-key = "jpcrp_cor:TotalAssetsUSGAAPSummaryOfBusinessResults"
-context_ref = "Prior1QuarterInstant"
-current_q1_assets = edinet_xbrl_object.get_data_by_context_ref(key, context_ref).get_value()
+# key = "jpcrp_cor:TotalAssetsUSGAAPSummaryOfBusinessResults"
+# context_ref = "Prior1QuarterInstant"
+# current_q1_assets = edinet_xbrl_object.get_data_by_context_ref(key, context_ref).get_value()
 
-print(current_q1_assets)
+# 1e
+# https://www.fsa.go.jp/search/20190228.html
+for key in edinet_xbrl_object.get_keys():
+    values = []
+    for value in edinet_xbrl_object.get_data_list(key):
+        values.append(value.get_value())
+    print(key, values)
