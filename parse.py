5557af4e322ccd5fdcfb8a3d68c9fb419655acb8
YoshihiroUeki <pirori67@gmail.com>
2019-10-30 23:43:13 +0900

    kadai collected

--- parse.py
+++ parse.py
@@ -4,8 +4,7 @@ from edinet_xbrl.edinet_xbrl_parser import EdinetXbrlParser
 parser = EdinetXbrlParser()
 
 ## parse xbrl file and get data container
-xbrl_file_path = "C:\\edinet_xbrl\\xbrl\\jpcrp040300-q1r-001_E01777-000_2019-06-30_01_2019-08-05.xbrl"
-edinet_xbrl_object = parser.parse_file(xbrl_file_path)
+# xbrl_file_path = "C:\\edinet_xbrl\\xbrl\\jpcrp040300-q1r-001_E01777-000_2019-06-30_01_2019-08-05.xbrl"
 
 # key = "jpcrp_cor:TotalAssetsUSGAAPSummaryOfBusinessResults"
 # context_ref = "Prior1QuarterInstant"
@@ -19,8 +18,23 @@ edinet_xbrl_object = parser.parse_file(xbrl_file_path)
 #         values.append(value.get_value())
 #     print(key, values)
 
-for key in edinet_xbrl_object.get_keys():
+# for key in edinet_xbrl_object.get_keys():
+#     for value in edinet_xbrl_object.get_data_list(key):
+#         v_str = value.get_value()
+#         if "対処すべき課題" in str(v_str):
+#             print(key, v_str)
+
+xbrl_file_path = "C:\\edinet_xbrl\\xbrl\\jpcrp030000-asr-001_E01777-000_2019-03-31_02_2019-08-01.xbrl"
+edinet_xbrl_object = parser.parse_file(xbrl_file_path)
+
+targetText = "BusinessPolicyBusinessEnvironmentIssuesToAddressEtcTextBlock".lower()
+issueKeys = {key for key in edinet_xbrl_object.get_keys() if str(key).__contains__(targetText)}
+
+for key in issueKeys:
     for value in edinet_xbrl_object.get_data_list(key):
-        v_str = value.get_value()
-        if "対処すべき課題" in str(v_str):
-            print(key, v_str)
+        print(value.get_value())
+
+# TODO
+# 1. fileに書き出す。html形式
+# 2. font-family: &apos;MS Mincho&apos;; 消す
+# 3. html, head周りを付与する
