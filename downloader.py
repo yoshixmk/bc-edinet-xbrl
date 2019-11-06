d0f491e96b508cb1ffd507201b0b0c9c15bfa713
YoshihiroUeki <pirori67@gmail.com>
2019-11-07 00:19:01 +0900

    added edinet code list file. loop in codes

--- downloader.py
+++ downloader.py
@@ -2,11 +2,10 @@ from edinet_xbrl.edinet_xbrl_downloader import EdinetXbrlDownloader
 
 
 class Downloader(object):
-    def __init__(self, ticker, workspace):
+    def __init__(self, ticker_or_edinet_code, workspace):
         self.xbrl_downloader = EdinetXbrlDownloader()
-        # set a ticker you want to download xbrl file
-        self.ticker = ticker  # test sony "6758"
+        self.ticker_or_edinet_code = ticker_or_edinet_code  # sony "6758" or Edinet Code
         self.target_dir = workspace.downloader_output_dir
 
     def execute(self):
-        self.xbrl_downloader.download_by_ticker(self.ticker, self.target_dir)
+        self.xbrl_downloader.download_by_ticker(self.ticker_or_edinet_code, self.target_dir)
--- main.py
+++ main.py
@@ -2,19 +2,27 @@ from downloader import Downloader
 from extract_parser import ExtractParser
 from models import workspace
 
+
+def getEdinetCodeList():
+    return [line.rstrip('\n') for line in open("public\\edinet_code_list.txt")]
+
+
 if __name__ == '__main__':
-    target_ticker = "6758"
 
-    workspace = workspace.Workspace(
-        "C:\\edinet_xbrl\\xbrl\\" + target_ticker,
-        "C:\\edinet_xbrl\\issues\\" + target_ticker
-    )
+    for edinet_code in getEdinetCodeList():
 
-    workspace.mkdir()
+        print("Start By Edinet Code: ", edinet_code)
+        w = workspace.Workspace(
+            "C:\\edinet_xbrl\\xbrl\\{}\\".format(edinet_code),
+            "C:\\edinet_xbrl\\issues\\{}\\".format(edinet_code)
+        )
 
-    downloader = Downloader(target_ticker, workspace)
-    downloader.execute()
+        w.mkdir()
 
-    parser = ExtractParser("BusinessPolicyBusinessEnvironmentIssuesToAddressEtcTextBlock", workspace)
-    parser.execute()
+        print("Downloading...")
+        downloader = Downloader(edinet_code, w)
+        downloader.execute()
 
+        print("Parsing...")
+        parser = ExtractParser("BusinessPolicyBusinessEnvironmentIssuesToAddressEtcTextBlock", w)
+        parser.execute()
